a9ae0a4f08143aa78493fe5f63c73c2527322181
SuperInstance <superinstance@users.noreply.github.com>
2026-06-09 01:48:30 -0800

    chore: add .gitignore, remove build artifacts

--- target/debug/examples/tutorial-dc7989b0dc0b0e01.d
+++
@@ -1,5 +0,0 @@
-/tmp/docs-tropical-geometry/target/debug/examples/tutorial-dc7989b0dc0b0e01.d: examples/tutorial.rs
-
-/tmp/docs-tropical-geometry/target/debug/examples/libtutorial-dc7989b0dc0b0e01.rmeta: examples/tutorial.rs
-
-examples/tutorial.rs:
